c3f8e04d733f7a8d11be03abb225259c65e9ad9c
luissian <chapado.l@gmail.com>
2020-12-18 22:51:21 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,3 +8,7 @@ source_suffix = ['.rst', '.md']
 
 master_doc = 'index'
 project = u'iSkyLIMS'
+copyright = u'2021, Luis Chapado , Sara Monzón'
+author = u'Luis Chapado , Sara Monzón'
+
+github_doc_root = 'https://github.com/luissian/iSkyLIMS_documentation/docs/'
