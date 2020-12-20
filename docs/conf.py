4457947d2fe18789b962fd8ecf1315e15a8ed232
luissian <chapado.l@gmail.com>
2020-12-21 00:42:00 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -207,7 +207,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'Recommonmarkdoc'
+htmlhelp_basename = 'iSkyLIMSSampledoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -229,8 +229,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'Recommonmark.tex', u'Recommonmark Documentation',
-   u'Lu Zero, Eric Holscher, and contributors', 'manual'),
+  (master_doc, 'iSkyLIMS.tex', u'iSkyLIMS Documentation',
+   u'Sara Monzon, Isabel Cuesta and Luis Chapado', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -259,7 +259,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'recommonmark', u'Recommonmark Documentation',
+    (master_doc, 'iSkyLIMS', u'iSkyLIMS Documentation',
      [author], 1)
 ]
 
@@ -273,8 +273,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'Recommonmark', u'Recommonmark Documentation',
-   author, 'Recommonmark', 'One line description of project.',
+  (master_doc, 'iSkyLIMS', u'iSkyLIMs Documentation',
+   author, 'iSkyLIMS', 'One line description of project.',
    'Miscellaneous'),
 ]
 
